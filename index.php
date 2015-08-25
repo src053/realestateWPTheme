09b1fbdb9eda9d49e994451e01db2ab17203d583
Matthew Hellen <src053@gmail.com>
2015-08-25 04:56:02 -0700

    change to the index and style

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php ?>
+<?php 
+
+if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        
+    <?php endwhile;
+else :
+    echo '<p>No content found</p>'
+?>
